c02d5723d9cbf250b031d51887265bafcd875420
8MarkOut <wudixjh@sina.com>
2017-06-05 16:13:10 +0800

    location

--- GDPLS/pages/location/location.js
+++ GDPLS/pages/location/location.js
@@ -1,9 +1,13 @@
 // location.js
+var app = getApp();
+var request = require('../../utils/util').request;
+
 Page({
   data: {
-  
+    location: null
   },
   onLoad: function (options) {
-  
+    app.getLocation(() => this.setData({ location: app.globalData.address }));
+    console.log(location);
   }
 })
